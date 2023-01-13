19386f4e49857001d2c786e746b00a6d0248edd6
FlorianKomnino <florian.komnino@outlook.it>
2023-01-13 13:01:58 +0100

    setup repo, vue + vite + sass + axios + store.js

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import axios from 'axios';
+import { store } from './store.js';
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  data() {
+    return {
+      store,
+    }
+  },
+
+  components: {
+    Header,
+    Main,
+    Footer,
+  }
+}
+</script>
+
+<template>
+  <h1>
+    hello Vue
+  </h1>
+
+  <Header />
+  <Main />
+  <Footer />
+</template>
+
+<style scoped>
+
+</style>
